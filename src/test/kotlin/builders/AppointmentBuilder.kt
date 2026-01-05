b7a6a1c7f454c959aabb6f8a083c0d20bc23f0ff
solalcaraz <alcarazmsol@gmail.com>
2026-01-05 12:52:01 -0300

    feat: add Agenda entity with availability validation and tests + builder

--- src/test/kotlin/builders/AppointmentBuilder.kt
+++ src/test/kotlin/builders/AppointmentBuilder.kt
@@ -1,5 +1,7 @@
 package builders
 
+import domain.Patient
+import domain.Service
 import domain.appointment.Appointment
 import java.time.LocalDateTime
 
@@ -9,6 +11,8 @@ public class AppointmentBuilder {
     private var startDateTime = LocalDateTime.now().plusDays(1)
     private var patientNote: String? = null
 
+    fun withService(service: Service) = apply { this.service = service }
+    fun withPatient(patient: Patient) = apply { this.patient = patient }
     fun withStartDateTime(dateTime: LocalDateTime) = apply { this.startDateTime = dateTime }
     fun withPatientNote(note: String) = apply { this.patientNote = note }
 
