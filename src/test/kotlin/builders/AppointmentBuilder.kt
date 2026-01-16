8df4c64477fdb18f0aad182df48d7a49f6a182fb
solalcaraz <alcarazmsol@gmail.com>
2026-01-16 18:44:39 -0300

    refactor: split startDateTime into startDate and startTime in AppointmentBuilder

--- src/test/kotlin/builders/AppointmentBuilder.kt
+++ src/test/kotlin/builders/AppointmentBuilder.kt
@@ -3,18 +3,21 @@ package builders
 import domain.Patient
 import domain.Service
 import domain.appointment.Appointment
-import java.time.LocalDateTime
+import java.time.LocalDate
+import java.time.LocalTime
 
 public class AppointmentBuilder {
     private var service = ServiceBuilder().build()
     private var patient = PatientBuilder().build()
-    private var startDateTime = LocalDateTime.now().plusDays(1)
+    private var startDate = LocalDate.now().plusDays(1)
+    private var startTime = LocalTime.now()
     private var patientNote: String? = null
 
     fun withService(service: Service) = apply { this.service = service }
     fun withPatient(patient: Patient) = apply { this.patient = patient }
-    fun withStartDateTime(dateTime: LocalDateTime) = apply { this.startDateTime = dateTime }
-    fun withPatientNote(note: String) = apply { this.patientNote = note }
+    fun withStartDate(date: LocalDate) = apply { this.startDate = date }
+    fun withStartTime(time: LocalTime) = apply { this.startTime = time }
+    fun withPatientNote(note: String?) = apply { this.patientNote = note }
 
-    fun build(): Appointment = Appointment(service, patient, startDateTime, patientNote)
+    fun build(): Appointment = Appointment(service, patient, startDate, startTime, patientNote)
 }
